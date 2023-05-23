e2407a5d09a8caf6f1a55883e38f08067c9bc9e1
Den <Polk300i@mail.ru>
2023-05-23 17:48:15 +0300

    Програма для диления двух рандомных чисел

--- Examples004_Div/Program.cs
+++ Examples004_Div/Program.cs
@@ -1,3 +1,5 @@
-﻿double numbersA = 12;
-double numbersB = 5;
+﻿double numbersA = new Random().Next(1, 10);
+Console.WriteLine(numbersA);
+double numbersB = new Random().Next(1, 10);
+Console.WriteLine(numbersB);
 Console.WriteLine(numbersA / numbersB);
