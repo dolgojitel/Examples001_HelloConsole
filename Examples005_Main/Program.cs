b4402bb7ee0637a8e3be352184f9ac17777d0426
Den <Polk300i@mail.ru>
2023-05-23 18:34:52 +0300

    Не решеная проблемв с терминалом else

---
+++ Examples005_Main/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Видите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Ян");
+{
+    Console.WriteLine("Крутое имя");
+}
+else
+{
+    Console.Write("Привет пользователь");
+    Console.WriteLine(sername);
+}
